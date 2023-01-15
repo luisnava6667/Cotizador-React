16100f7c1cb4170b9a4c7f441a201b4d66b3d05e
luisnava6667 <luisnavarro19890603@gmail.com>
2023-01-15 09:55:31 -0300

    first commit

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -17,7 +17,7 @@ const Resultado = () => {
   if (resultado === 0) return null;
   return (
     <div className="bg-gray-100 text-center mt-5 shadow">
-      <h2 className="text-gray-600 font-black text-3xl">Resume</h2>
+      <h2 className="text-gray-600 font-black text-3xl">Resumen</h2>
       <p className="my-2">
         <span className="font-bold">Marca: </span>
         {nombreMarca.nombre}
